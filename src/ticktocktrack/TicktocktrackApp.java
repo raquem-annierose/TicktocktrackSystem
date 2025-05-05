ca581531a32bab374e816c4789891ae4fbf798d4
raquem-annierose <annierosesraquem@iskolarngbayan.pup.edu.ph>
2025-05-05 16:45:36 +0800

    update

---
+++ src/ticktocktrack/TicktocktrackApp.java
@@ -0,0 +1,10 @@
+package ticktocktrack;
+
+import ticktocktrack.gui.Homepage;
+import javafx.application.Application;
+
+public class TicktocktrackApp {
+    public static void main(String[] args) {
+        Application.launch(Homepage.class, args);
+    }
+}
