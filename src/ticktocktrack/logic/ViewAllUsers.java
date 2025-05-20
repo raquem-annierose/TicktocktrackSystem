40782e98d854a6b77c51967847bc71f4ba75fbdf
raquem-annierose <annierosesraquem@iskolarngbayan.pup.edu.ph>
2025-05-21 05:34:19 +0800

    view all users and manage users (delete, edit)

--- src/ticktocktrack/logic/ViewAllUsers.java
+++ src/ticktocktrack/logic/ViewAllUsers.java
@@ -1,10 +1,29 @@
-package ticktocktrack.logic;
+package ticktocktrack.database;
 
-public class ViewAllUsers {
+import java.util.List;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+import ticktocktrack.logic.UsersModel;
 
-	}
+public class DatabaseViewAllUsers {
 
+    public static void main(String[] args) {
+
+        System.out.println("Admins:");
+        List<UsersModel> admins = UserDAO.getAdmins();
+        for (UsersModel admin : admins) {
+            System.out.println(admin);
+        }
+
+        System.out.println("\nFaculty:");
+        List<UsersModel> faculty = UserDAO.getTeachers();
+        for (UsersModel teacher : faculty) {
+            System.out.println(teacher);
+        }
+
+        System.out.println("\nStudents:");
+        List<UsersModel> students = UserDAO.getStudents();
+        for (UsersModel student : students) {
+            System.out.println(student);
+        }
+    }
 }
