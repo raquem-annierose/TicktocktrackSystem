e3082621121a331a4e89dd56c5a0867287dedd83
reduta-paulbenidict <kenjicanpos829@gmail.com>
2025-05-24 15:54:01 +0800

    add StudentIndividualReportPanel class

---
+++ src/ticktocktrack/gui/StudentIndividualReportPanel.java
@@ -0,0 +1,62 @@
+package ticktocktrack.gui;
+
+import javafx.geometry.Insets;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import ticktocktrack.database.DatabaseIndividualReport;
+import ticktocktrack.logic.Student;
+
+public class StudentIndividualReportPanel {
+
+    public static Pane createPanel(Student loggedInStudent) {
+        Pane centerPanel = new Pane();
+        centerPanel.setPrefSize(1300, 750);
+        centerPanel.setStyle("-fx-background-color: #EEF5F9; -fx-border-color: #cccccc; -fx-border-width: 1px;");
+
+        // Background shadow
+        String shadowPath = StudentIndividualReportPanel.class.getResource("/resources/SHADOW.png").toExternalForm();
+        ImageView shadowView = new ImageView(new Image(shadowPath));
+        shadowView.setFitWidth(1300);
+        shadowView.setFitHeight(250);
+        shadowView.setLayoutX(0);
+        shadowView.setLayoutY(-115);
+
+        // Title
+        Text title = new Text("My Attendance Summary");
+        title.setFont(Font.font("Poppins", FontWeight.BOLD, 36));
+        title.setFill(Color.web("#02383E"));
+        title.setLayoutX(50);
+        title.setLayoutY(70);
+
+        VBox studentSummaryContainer = new VBox(15);
+        studentSummaryContainer.setPadding(new Insets(40, 20, 20, 50));
+        studentSummaryContainer.setLayoutY(120);
+        studentSummaryContainer.setLayoutX(0);
+
+        studentSummaryContainer.setPrefWidth(1200);
+        studentSummaryContainer.setStyle("-fx-background-color: white; -fx-background-radius: 10;");
+        
+        // Display basic student information
+        Label nameLabel = new Label(loggedInStudent.getLastName() + ", " +
+                                    loggedInStudent.getFirstName() + " " +
+                                    loggedInStudent.getMiddleName());
+        nameLabel.setFont(Font.font("Poppins", FontWeight.BOLD, 20));
+        nameLabel.setTextFill(Color.web("#02383E"));
+
+        Label yearLabel = new Label("Year Level: " + loggedInStudent.getYearLevel());
+        yearLabel.setFont(Font.font("Poppins", 14));
+        yearLabel.setTextFill(Color.web("#555555"));
+
+        // Attendance summary from database
+        
+
+        centerPanel.getChildren().addAll(title, studentSummaryContainer, shadowView);
+        return centerPanel;
+    }
+}
