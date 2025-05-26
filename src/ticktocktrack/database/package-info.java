5d34a39456e774c9e4b1f2d55713010d9ed674ef
Kenji Campos <kenjicanpos829@gmail.com>
2025-05-26 11:28:35 +0800

    add package-info.java

---
+++ src/ticktocktrack/database/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * Contains utility classes and functions for database
+ * operations in the TickTockTrack application.
+ *
+ * <p>This package includes DAO classes, connection managers,
+ * and SQL query builders for seamless database interactions.</p>
+ *
+ * @author OogaBooga
+ * @version 1.0
+ */
+package ticktocktrack.database;
